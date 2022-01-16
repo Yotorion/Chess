9d1a4adba58c4ef548d10aa21851eb3ee74ef627
Yotorion <56510267+Yotorion@users.noreply.github.com>
2022-01-16 03:05:44 -0300

    Position class created

---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+	
+	private Integer row;
+	private Integer column;
+	
+	public Position () {};
+	
+	public Position (Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Integer getRow () {
+		return this.row;
+	}
+
+	public Integer getColumn () {
+		return this.column;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Position on the board \n");
+		sb.append("Row: " + this.row + "\n");
+		sb.append("Column: " + this.column);
+		return sb.toString();
+	}
+	
+}
