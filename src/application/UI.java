74588ea7741cb2361fa6531ccdd51fa09478c2b1
Yotorion <56510267+Yotorion@users.noreply.github.com>
2022-01-26 01:32:43 -0300

    King & Rook + Initial Board Method

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,12 +5,14 @@ import chess.ChessPiece;
 public class UI {
 	
 	public static void printBoard (ChessPiece[][] pieces) {
+		
 		for(int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for(int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
-			System.out.println();
+			
+			System.out.println("");
 		}
 		System.out.println("  a b c d e f g h");
 	}
@@ -20,7 +22,7 @@ public class UI {
 			System.out.print("-");
 		}
 		else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
 		
 		System.out.print(" ");
